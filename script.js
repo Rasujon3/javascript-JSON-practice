d564bc3c5a19fe1082596d1f3fadb5f84ff6ed84
Ruhul Amin Sujon <rasujon3@gmail.com>
2021-05-30 16:01:03 +0600

    03. JSON Data Types - JavaScript by Simanta Paul
    03. JSON Data Types - JavaScript by Simanta Paul

--- script.js
+++ script.js
@@ -1,13 +1,24 @@
-var student = {
-    name: "Sujon",
-    age: 23,
-    hometown: "Gaibandha"
+// https://jsonlint.com
+// string
+// number
+// object (JSON object)
+// array
+// boolean
+// null
 
-};
+// JSON doesn't suppoer undefined, date, function
+var person = {
+    name: "Sujon", // string
+    age: 23, // number
+    hometown: "Gaibandha",
+    married: false,// bool
+    dob: "1997-05-12",//date
+    test_null: null,//null
+    test_underfined: undefined,//undefined
+    greet: function(){//function
+        console.log(`Hello ${this.name}`);
+    }
 
-var student_json = JSON.stringify(student);
-
-console.log(student_json);
-
-var student_new = JSON.parse(student_json);
-console.log(student_new);
+}
+var person_json = JSON.stringify(person);
+console.log(person_json);
